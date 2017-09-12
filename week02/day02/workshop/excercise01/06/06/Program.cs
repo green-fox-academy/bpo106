55e3ddc687fd099cbd252fe2e5f6d2d46f327dba
bpo106 <bpo106bkv@gmail.com>
2017-09-12 14:05:41 +0200

    6th task

--- week02/day02/workshop/excercise01/06/06/Program.cs
+++ week02/day02/workshop/excercise01/06/06/Program.cs
@@ -10,15 +10,18 @@ namespace _06
     {
         static void Main(string[] args)
         {
-            //string[] mystrings = { "First" };
-            //string[] mystrings = { "First", "Second" };
-            //string[] mystrings = { "First", "Second", "Third" };
-            string[] mystrings = { "First", "Second", "Third", "Fourth" };
-            printer(mystrings);
+            string[] mystrings1 = { "First" };
+            string[] mystrings2 = { "First", "Second" };
+            string[] mystrings3 = { "First", "Second", "Third" };
+            string[] mystrings4 = { "First", "Second", "Third", "Fourth" };
+            printer(mystrings1);
+            printer(mystrings2);
+            printer(mystrings3);
+            printer(mystrings4);
             Console.ReadLine();
         }
 
-        static void printer(string[] strings)
+        static void printer(params string[] strings)
         {
             for (int i = 0; i < strings.Length; i++) Console.WriteLine(strings[i]);
         }
