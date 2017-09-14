7d7fbbebee2ef47ba12a03d24933e78d9b4b65bc
bpo106 <bpo106bkv@gmail.com>
2017-09-14 14:27:04 +0200

    Playable without sumbitting other blocks with 0 neighbouring mines

--- week02/day03/workshop/minesweeper/minesweeper/minesweeper/Program.cs
+++ week02/day03/workshop/minesweeper/minesweeper/minesweeper/Program.cs
@@ -8,12 +8,25 @@ namespace minesweeper
 {
     class Program
     {
-        static string[,] PreField (int height, int width, string s)
+        static int[,] PreField (int height, int width)
         {
-            string[,] field = new string[height, width];
+            int[,] field = new int[height, width];
             for ( int i = 0; i < height;i++ )
             {
                 for ( int j = 0; j < width ; j++ )
+                {
+                    field[i, j] = 0;
+                }
+            }
+            return field;
+        }
+
+        static string[,] PreFieldSee(int height, int width, string s)
+        {
+            string[,] field = new string[height, width];
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
                 {
                     field[i, j] = s;
                 }
@@ -21,18 +34,31 @@ namespace minesweeper
             return field;
         }
 
-        static string[,] Mineplacer (string[,] field, int minenum, int xcoord, int ycoord)
+        static int[,] Mineplacer (int[,] field, int minenum, int xcoord, int ycoord)
         {
-            for ( int i = 0; i < minenum; )
+            List<int[]> list = new List<int[]>();
+            for (int i = 0; i < field.GetLength(1); i++)
             {
-                Random random = new Random();
-                int a = random.Next(0, field.GetLength(0));
-                int b = random.Next(0, field.GetLength(1));
-                if (field[a,b] == "0" && a != ycoord && b != xcoord)
+                for (int j = 0; j < field.GetLength(0); j++)
                 {
-                    field[a, b] = "*";
-                    i++;
-                }
+                    if (i != xcoord || j != ycoord)
+                    {
+                        int[] array = new int[2];
+                        array[0] = i;
+                        array[1] = j;
+                        list.Add(array);
+                    }
+                }                    
+            }
+
+            Random randommine = new Random();
+
+            while (minenum > 0)
+            {
+                int choose = randommine.Next(0, list.Count - 1);
+                field[(list[choose])[0], (list[choose])[1]] = 9;
+                list.RemoveAt(choose);
+                minenum--;
             }
             return field;
         }
@@ -49,27 +75,22 @@ namespace minesweeper
             }
         }
 
-        static string[,] FieldCounter (string[,] field)
+        static int[,] FieldCounter (int[,] field)
         {
             for (int i = 0; i < field.GetLength(0); i++)
             {
                 for (int j = 0; j < field.GetLength(1); j++)
                 {
-                    if (field[i, j] == "0")
+                    if (field[i, j] == 0)
                     {
-                        int k = 0;
-
-                        if (i > 0 && j > 0 && field[i - 1, j - 1] == "*") k++;
-                        if (i > 0 && j < field.GetLength(1) - 1 && field[i - 1, j + 1] == "*") k++;
-                        if (i < field.GetLength(0) - 1 && j < field.GetLength(1) - 1 && field[i + 1, j + 1] == "*") k++;
-                        if (i < field.GetLength(0) - 1 && j > 0 && field[i + 1, j - 1] == "*") k++;
-                        if (i < field.GetLength(0) - 1 && field[i + 1, j] == "*") k++;
-                        if (j < field.GetLength(1) - 1 && field[i, j + 1] == "*") k++;
-                        if (i > 0 && field[i - 1, j] == "*") k++;
-                        if (j > 0 && field[i, j - 1] == "*") k++;
-
-                        if (k != 0) field[i, j] = k.ToString();
-                        else field[i, j] = "-";
+                        if (i > 0 && j > 0 && field[i - 1, j - 1] == 9) field[i, j]++;
+                        if (i > 0 && j < field.GetLength(1) - 1 && field[i - 1, j + 1] == 9) field[i, j]++;
+                        if (i < field.GetLength(0) - 1 && j < field.GetLength(1) - 1 && field[i + 1, j + 1] == 9) field[i, j]++;
+                        if (i < field.GetLength(0) - 1 && j > 0 && field[i + 1, j - 1] == 9) field[i, j]++;
+                        if (i < field.GetLength(0) - 1 && field[i + 1, j] == 9) field[i, j]++;
+                        if (j < field.GetLength(1) - 1 && field[i, j + 1] == 9) field[i, j]++;
+                        if (i > 0 && field[i - 1, j] == 9) field[i, j]++;
+                        if (j > 0 && field[i, j - 1] == 9) field[i, j]++;
                     }
                 }
             }
@@ -80,8 +101,8 @@ namespace minesweeper
         {
             Random random = new Random();
             bool amIdead = false;
-            int height = random.Next(5, 15);
-            int width = random.Next(5, 15);
+            int height = 4;//random.Next(5, 15);
+            int width = 4;//random.Next(5, 15);
 
             Console.WriteLine("It's time to play the game!");
             Console.WriteLine("Height: {0}", height);
@@ -103,7 +124,7 @@ namespace minesweeper
             }
 
             Console.Clear();
-            string[,] fieldSee = PreField(height, width, "■");
+            string[,] fieldSee = PreFieldSee(height, width, "■");
             FieldDrawer(fieldSee);
             int xcoord = 15;
             int ycoord = 15;
@@ -119,7 +140,7 @@ namespace minesweeper
                 ycoord = Int32.Parse(Console.ReadLine());
             }
 
-            string[,] field = PreField(height, width, "0");
+            int[,] field = PreField(height, width);
             field = Mineplacer(field, mines, xcoord, ycoord);
             field = FieldCounter(field);
 
@@ -127,15 +148,29 @@ namespace minesweeper
 
             while ((count > 0))
             {
-                if (field[ycoord, xcoord] == "*")
+                if (field[ycoord, xcoord] == 9)
                 {
                     count = 0;
+                    amIdead = true;
+                    for (int i = 0; i < field.GetLength(1); i++ )
+                    {
+                        for (int j = 0; j < field.GetLength(0); j++)
+                        {
+                            if (field[i, j] == 9) fieldSee[i, j] = "*";
+                        }
+                    }
+                    Console.Clear();
+                    FieldDrawer(fieldSee);
                     Console.WriteLine("Killed By Death\nBtw R.I.P. Lemmy :(");
                 }
                 else
                 {
                     Console.Clear();
-                    fieldSee[ycoord, xcoord] = field[ycoord, xcoord];
+                    if (field[ycoord,xcoord] == 0)
+                        fieldSee[ycoord, xcoord] = "-";
+                    else
+                        fieldSee[ycoord, xcoord] = field[ycoord, xcoord].ToString();
+
                     FieldDrawer(fieldSee);
 
                     Console.Write("Set the X coordinate: ");
@@ -144,10 +179,19 @@ namespace minesweeper
                     ycoord = Int32.Parse(Console.ReadLine());
 
                     count--;
-                    if (count == 0) Console.WriteLine("We only live to win!");
+                }
+                if ((count == 0) && (amIdead == false))
+                {
+                    if (field[ycoord, xcoord] == 0)
+                        fieldSee[ycoord, xcoord] = "-";
+                    else
+                        fieldSee[ycoord, xcoord] = field[ycoord, xcoord].ToString();
+                    FieldDrawer(fieldSee);
+
+                    Console.Clear();
+                    Console.WriteLine("We only live to win!");
                 }
             }
-
             Console.ReadLine();
         }
     }
