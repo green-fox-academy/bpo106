abddbb201e736c5aaf6670e9b2c0af4e96ed2318
bpo106 <bpo106bkv@gmail.com>
2017-09-15 14:10:50 +0200

    Fourth task done

---
+++ week02/day05/workshop/excercise01/05/05/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05
+{
+    class Program
+    {
+        
+        static int Josephus (List<int> list)
+        {
+            
+            while (list.Count > 2)
+            {
+                int temp = list[0];
+                list.Remove(list[0]);
+                list.Remove(list[0]);
+                list.Add(temp);
+            }
+            
+            return list[0];
+        }
+
+        static void Main(string[] args)
+        {
+            int input;
+            input = Int32.Parse(Console.ReadLine());
+            List<int> list = new List<int>();
+            for (int i = 0; i < input; i++)
+            {
+                list.Add(i + 1);
+            }
+
+            Console.WriteLine(Josephus(list));
+            Console.ReadLine();
+        }
+    }
+}
