87b0c74a3f9dfa8ff2fd938381d1e4b4fb9ecede
bpo106 <bpo106bkv@gmail.com>
2017-09-15 14:24:19 +0200

    Sixth task done

---
+++ week02/day05/workshop/excercise01/06/06/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int inputnum = Int32.Parse(input);
+            int number = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                int k = 1;
+                for (int j = 0; j < input.Length; j++)
+                {
+                    k *= (int)char.GetNumericValue(input[i]);
+                }
+                number += k;
+            }
+
+            if (number == inputnum) Console.WriteLine(number + " is an Armstrong number.");
+            else Console.WriteLine(inputnum + " is not an Armstrong number.");
+            Console.ReadLine();
+        }
+    }
+}
