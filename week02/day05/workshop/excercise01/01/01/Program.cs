aae7d0a64c0af0c8d18affe52117f4de8bd7bb0b
bpo106 <bpo106bkv@gmail.com>
2017-09-15 09:52:51 +0200

    First task done

---
+++ week02/day05/workshop/excercise01/01/01/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01
+{
+    class Program
+    {
+        static bool IsAnagram(string input1, string input2)
+        {
+            char[] word1 = input1.ToArray();
+            Array.Sort(word1);
+            char[] word2 = input2.ToArray();
+            Array.Sort(word2);
+            bool anagram = true;
+
+            if (word1.Length == word2.Length)
+            {
+                for (int i = 0; i< word1.Length; i++)
+                {
+                    if (word1[i] != word2[i]) anagram = false;
+                }
+            }
+
+            else anagram = false;
+
+            return anagram;
+        }
+
+        static void Main(string[] args)
+        {
+            string input1 = "eleven plus two";
+            string input2 = "twelve plus one";
+
+            Console.WriteLine(IsAnagram(input1, input2));
+            Console.ReadLine();
+        }
+    }
+}
