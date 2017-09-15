b02804f779489dfda7d8084ebb7c855f35271f6c
bpo106 <bpo106bkv@gmail.com>
2017-09-15 10:08:42 +0200

    Second task done

---
+++ week02/day05/workshop/excercise01/02/02/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02
+{
+    class Program
+    {
+        static string CreatePalindrome (string input)
+        {
+            List<char> list = new List<char>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                list.Add(input[i]);
+            }
+            for (int i =input.Length; i>0; i--)
+            {
+                list.Add(input[i - 1]);
+            }
+
+            return string.Join("", list.ToArray());
+        }
+
+        static void Main(string[] args)
+        {
+            string input = "random";
+            string output = CreatePalindrome(input);
+
+            for (int i = 0; i < output.Length; i++)
+            {
+                Console.Write(output[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
