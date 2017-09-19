066739d6fa03f393d9ce360c6e36c1481a95752a
bpo106 <bpo106bkv@gmail.com>
2017-09-19 12:02:00 +0200

    Second and third excercises done.

---
+++ week03/day02/workshop/excercise02/02/02/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace _02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"my-file.txt";
+
+            try
+            {
+                string[] content = File.ReadAllLines(path);
+                for (int i = 0; i < content.Length; i++)
+                {
+                    Console.WriteLine(content[i]);
+                }
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine("Unable to read file: my-file.txt");
+            }
+
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ week03/day02/workshop/excercise02/03/03/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace _03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"random.txt";
+
+            try
+            {
+                string[] content = File.ReadAllLines(path);
+                Console.WriteLine(content.Length);
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine("0");
+            }
+
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
