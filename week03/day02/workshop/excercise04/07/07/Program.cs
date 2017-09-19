3aae6c028a30ec1c3670c692ab45260dcd6903d5
bpo106 <bpo106bkv@gmail.com>
2017-09-19 15:23:38 +0200

    07 done

---
+++ week03/day02/workshop/excercise04/07/07/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace _07
+{
+    class Program
+    {
+        static void Simplify (string path)
+        {
+            string[] content = File.ReadAllLines(path);
+
+            for (int i = 0; i < content.Length; i++ )
+            {
+                var text = new List<char>(content[i]);
+
+                for (int j = 0; j < text.Count- 1; j++)
+                {
+                    text.RemoveAt(j);
+                }
+
+                content[i] = string.Join("", text.ToArray());
+            }
+            File.WriteAllLines(path, content);
+        }
+
+        static void Main(string[] args)
+        {
+            string path = @"duplicated-chars.txt";
+            Simplify(path);
+            Console.ReadLine();
+        }
+    }
+}
