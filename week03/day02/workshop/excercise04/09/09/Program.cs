25fc70bcff31e65efcc0475934e8a6c3ef72dcaa
bpo106 <bpo106bkv@gmail.com>
2017-09-19 15:47:27 +0200

    haaaaaaaaaands

---
+++ week03/day02/workshop/excercise04/09/09/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace _09
+{
+    class Program
+    {
+        static void Reverse(string path)
+        {
+            string[] content = File.ReadAllLines(path);
+
+            Array.Reverse(content);
+
+            File.WriteAllLines(path, content);
+        }
+
+        static void Main(string[] args)
+        {
+            string path = @"reversed-order.txt";
+            Reverse(path);
+            Console.ReadLine();
+        }
+    }
+}
