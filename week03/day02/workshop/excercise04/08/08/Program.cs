1bf35f6e08c05fff857cae490bb8f648f0bb9b26
bpo106 <bpo106bkv@gmail.com>
2017-09-19 15:30:00 +0200

    my hands are typing words

---
+++ week03/day02/workshop/excercise04/08/08/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace _08
+{
+    class Program
+    {
+        static void Reverse(string path)
+        {
+            string[] content = File.ReadAllLines(path);
+
+            for (int i = 0; i < content.Length; i++)
+            {
+                char[] array = content[i].ToCharArray();
+                Array.Reverse(array);
+                content[i] = new string(array);
+            }
+            File.WriteAllLines(path, content);
+        }
+
+        static void Main(string[] args)
+        {
+            string path = @"reversed-lines.txt";
+            Reverse(path);
+            Console.ReadLine();
+        }
+    }
+}
