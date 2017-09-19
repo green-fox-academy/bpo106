c738d004237d6ac0439b1b792e06580f4c2fc519
bpo106 <bpo106bkv@gmail.com>
2017-09-19 10:58:10 +0200

    First excercise done.

---
+++ week03/day02/workshop/excercise01/01/01/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01
+{
+    class Program
+    {
+        static void Divide (int input)
+        {
+            try
+            {
+                input = 10 / input;
+                Console.WriteLine(input);
+            }
+
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine("fail");
+            }
+
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Divide(Int32.Parse(Console.ReadLine()));
+        }
+    }
+}
