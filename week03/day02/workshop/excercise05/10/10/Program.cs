11ecadca3cec8bb70c6199050f6b03b92972b95f
bpo106 <bpo106bkv@gmail.com>
2017-09-19 17:06:40 +0200

    Fifth excercise done.

---
+++ week03/day02/workshop/excercise05/10/10/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace _10
+{
+    class Program
+    {
+        static void Decode (string path)
+        {
+            string[] content = File.ReadAllLines(path);
+
+            for (int i = 0; i < content.Length; i++)
+            {
+                char[] row = content[i].ToCharArray();
+                for (int j = 0; j < content[i].Length; j++)
+                {
+                    if (row[j] != ' ')
+                    {
+                        row[j] -= (char)1;
+                    }
+                }
+                content[i] = new string(row);
+            }
+            File.WriteAllLines(path, content);
+        }
+
+        static void Main(string[] args)
+        {
+            string str = @"encoded-lines.txt";
+            Decode(str);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ week03/day02/workshop/excercise05/11/11/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace _11
+{
+    class Program
+    {
+        static List<string>[] NumbersMaker (string[] rows)
+        {
+            List<string>[] numbers = new List<string>[rows.Length];
+            for (int i = 0; i < rows.Length; i++)
+            {
+                numbers[i] = rows[i].Split(';').ToList();
+                for (int j = 0; j < 11; j++)
+                {
+                    numbers[i].RemoveAt(0);
+                }
+            }
+            return numbers;
+        }
+
+        static int[] CountFiller (List<string>[] numbers)
+        {
+            int[] count = new int[90];
+
+            for (int i = 0; i < 90; i++)
+            {
+                count[i] = 0;
+            }
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    count[Int32.Parse(numbers[i][j]) - 1] += Int32.Parse(numbers[i][j]);
+                }
+            }
+            return count;
+        }
+
+        static int MaxCounter(int[] count)
+        {
+            int max = 0;
+            int index = 0;
+
+            for (int i = 0; i < 90; i++)
+            {
+                if (max < count[i])
+                {
+                    max = count[i];
+                    index = i + 1;
+                }
+            }
+
+            return index;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MaxCounter(CountFiller(NumbersMaker(File.ReadAllLines(@"otos.csv")))));
+
+            Console.ReadLine();
+        }
+    }
+}
