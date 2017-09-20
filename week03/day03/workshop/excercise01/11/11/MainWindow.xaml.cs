133a4e9e6a07f38d05a574036f0cd2b46b07f4f2
bpo106 <bpo106bkv@gmail.com>
2017-09-20 15:47:02 +0200

    11

---
+++ week03/day03/workshop/excercise01/11/11/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GreenFox;
+
+namespace _11
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+
+            for (int i = 1; i < 20; i++)
+            {
+                foxDraw.FillColor(Colors.MediumPurple);
+                foxDraw.DrawRectangle(10*i, 10*i, 10, 10);
+            }
+        }
+    }
+}
