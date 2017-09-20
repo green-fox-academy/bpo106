eae64f5931a21dc0e6101f6c1c4f917f84dddc28
bpo106 <bpo106bkv@gmail.com>
2017-09-20 11:30:31 +0200

    02

---
+++ week03/day03/workshop/excercise01/02/02/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GreenFox;
+
+namespace _02
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static void DrawRectangle(FoxDraw foxDraw)
+        {
+            var points = new List<Point>();
+            points.Add(new Point(100, 100));
+            points.Add(new Point(100, 200));
+            points.Add(new Point(200, 200));
+            points.Add(new Point(200, 100));
+
+            foxDraw.StrokeColor(Colors.Green);
+            foxDraw.DrawLine(points[0], points[1]);
+            foxDraw.StrokeColor(Colors.Blue);
+            foxDraw.DrawLine(points[1], points[2]);
+            foxDraw.StrokeColor(Colors.Red);
+            foxDraw.DrawLine(points[2], points[3]);
+            foxDraw.StrokeColor(Colors.Black);
+            foxDraw.DrawLine(points[3], points[0]);
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+            // draw a box that has different colored lines on each edge.
+            DrawRectangle(foxDraw);
+        }
+    }
+}
