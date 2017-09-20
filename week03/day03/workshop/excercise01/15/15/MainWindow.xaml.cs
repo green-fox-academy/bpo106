d3de0d449f9fa3c08e518b7801a78137ef41708b
bpo106 <bpo106bkv@gmail.com>
2017-09-20 16:53:56 +0200

    15

---
+++ week03/day03/workshop/excercise01/15/15/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GreenFox;
+
+namespace _15
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public void WidenedGape(FoxDraw foxDraw)
+        {
+            for (int i = 0; i <= 300; i+=20)
+            {
+                foxDraw.StrokeColor(Colors.Green);
+                foxDraw.DrawLine(0, i, i, 300);
+                foxDraw.StrokeColor(Colors.Purple);
+                foxDraw.DrawLine(i, 0, 300, i);
+            }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+
+            WidenedGape(foxDraw);
+        }
+    }
+}
