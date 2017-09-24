43ac01b3ec32cfe5d0d2453846aa97ce724f63f5
bpo106 <bpo106bkv@gmail.com>
2017-09-24 12:17:47 +0200

    homework

---
+++ week04/homework/00/00/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _00
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<List<string>> tree = new List<List<string>>();
+            tree.Add(new List<string> { "pine", "green", "20", "male" });
+            tree.Add(new List<string> { "yew", "green", "2", "female" });
+            tree.Add(new List<string> { "apple", "green", "4", "monoecious" });
+            tree.Add(new List<string> { "pear", "green", "15", "monoecious" });
+            tree.Add(new List<string> { "plum", "green", "7", "monoecious" });
+        }
+    }
+}
