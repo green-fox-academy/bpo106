d3a65518b269603399b7d384f8a85f0e1b977bde
bpo106 <bpo106bkv@gmail.com>
2017-09-26 01:18:41 +0200

    11 done

---
+++ week04/day01/workshop/excercise05/11/11/Sharpie.cs
@@ -0,0 +1,26 @@
+﻿namespace _11
+{
+    public class Sharpie
+    {
+        public string Color;
+        public float Width;
+        private float InkAmount = 100;
+
+        public void Use()
+        {
+            InkAmount--;
+        }
+
+        public bool IsPositive()
+        {
+            if (InkAmount == 0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
---
+++ week04/day01/workshop/excercise05/11/11/SharpieSet.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace _11
+{
+    public class SharpieSet
+    {
+        private List<Sharpie> Sharpies;
+
+        public SharpieSet()
+        {
+            Sharpies = new List<Sharpie>();
+        }
+
+        public bool CountUsable(Sharpie sharpie)
+        {
+            if (sharpie.IsPositive())
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void RemoveTrash(Sharpie sharpie)
+        {
+            if (!(CountUsable(sharpie)))
+            {
+                Sharpies.Remove(sharpie);
+            }
+        }
+    }
+}
