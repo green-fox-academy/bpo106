bbf869eca2a84bf97a182181d44153c69fd2e8a6
bpo106 <bpo106bkv@gmail.com>
2017-09-25 22:42:12 +0200

    06 done

---
+++ week04/day01/workshop/excercise03/06/06/Animal.cs
@@ -0,0 +1,32 @@
+﻿namespace _06
+{
+    public class Animal
+    {
+        private int Hunger;
+        private int Thirst;
+
+        Animal()
+        {
+            this.Hunger = 50;
+            this.Thirst = 50;
+        }
+
+        public void eat()
+        {
+            this.Hunger--;
+        }
+
+        public void drink()
+        {
+            this.Thirst--;
+        }
+
+        public void play()
+        {
+            this.Hunger++;
+            this.Thirst++;
+        }
+
+    }
+
+}
