dee95e5bfea41e1d10c0aa8dd6e757e3edabd785
bpo106 <bpo106bkv@gmail.com>
2017-09-25 22:21:23 +0200

    05 done

--- week04/day01/workshop/excercise02/05/05/Program.cs
+++ week04/day01/workshop/excercise02/05/05/Program.cs
@@ -1,15 +1,31 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace _05
+namespace FleetOfThings
 {
-    class Program
+    public class FleetOfThings
     {
-        static void Main(string[] args)
+
+
+        public static void Main(string[] args)
         {
+            var fleet = new Fleet();
+
+            Console.Write("1. ");
+            fleet.Add(new Thing("Get milk"));
+            Console.Write("2. ");
+            fleet.Add(new Thing("Remove the obstacles"));
+            Console.Write("3. ");
+            fleet.Add(new Thing("Stand up"));
+            Console.Write("4. ");
+            fleet.Add(new Thing("Eat lunch"));
+            Console.ReadLine();
+
+            // Create a fleet of things to have this output:
+            // 1. [ ] Get milk
+            // 2. [ ] Remove the obstacles
+            // 3. [x] Stand up
+            // 4. [x] Eat lunch
+            // Hint: You have to create a Print method also
         }
     }
 }
---
+++ week04/day01/workshop/excercise02/05/05/Thing.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FleetOfThings
+{
+    public class Thing
+    {
+        private string Name;
+        private bool Completed;
+
+        public Thing(string name)
+        {
+            this.Name = name;
+
+            if (this.Name == "Stand up" || this.Name == "Eat lunch")
+            {
+                this.Completed = true;
+                Console.WriteLine("[X] {0}", this.Name);
+            }
+            else
+            {
+                this.Completed = false;
+                Console.WriteLine("[ ] {0}", this.Name);
+            }
+        }
+    }
+}
