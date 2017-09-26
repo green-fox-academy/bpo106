d8cc92c31278b30987bcce9ccfcc4409020d29fd
bpo106 <bpo106bkv@gmail.com>
2017-09-26 15:57:48 +0200

    15 done

---
+++ week04/day01/workshop/excercise06/15/15/Tree.cs
@@ -0,0 +1,18 @@
+﻿namespace _15
+{
+    public class Tree
+    {
+        private string Type;
+        private string LeafColor;
+        private int Age;
+        private string Sex;
+
+        public Tree(string type, string leafColor, int age, string sex)
+        {
+            this.Type = type;
+            this.LeafColor = leafColor;
+            this.Age = age;
+            this.Sex = sex;
+        }
+    }
+}
