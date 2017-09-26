5737990afed65aaf36a7cc1541ea68f63f961c06
bpo106 <bpo106bkv@gmail.com>
2017-09-26 02:06:02 +0200

    12 done

---
+++ week04/day01/workshop/excercise05/12/12/Animal.cs
@@ -0,0 +1,36 @@
+﻿namespace _12
+{
+    public class Animal
+    {
+        private int Hunger;
+        private int Thirst;
+
+        Animal()
+        {
+            this.Hunger = 50;
+            this.Thirst = 50;
+        }
+
+        public void eat()
+        {
+            this.Hunger--;
+        }
+
+        public void drink()
+        {
+            this.Thirst--;
+        }
+
+        public void play()
+        {
+            this.Hunger++;
+            this.Thirst++;
+        }
+
+        public int GetHunger()
+        {
+            return Hunger;
+        }
+    }
+
+}
---
+++ week04/day01/workshop/excercise05/12/12/Farm.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace _12
+{
+    public class Farm
+    {
+        private List<Animal> Animals;
+        private int FreePlace;
+
+        public void Breed(Animal animal, int freePlace)
+        {
+            Animals.Add(animal);
+            freePlace--;
+        }
+
+        public Animal LessHungry()
+        {
+            int h = Animals[0].GetHunger();
+            for (int i = 1; i < Animals.Count; i++)
+            {
+                if (Animals[i].GetHunger() < Animals[h].GetHunger())
+                {
+                    h = i;
+                }
+            }
+            return Animals[h];
+        }
+
+        public void Slaughter(Animal animal, int freePlace)
+        {
+            Animals.Remove(animal);
+            freePlace++;
+        }
+    }
+}
