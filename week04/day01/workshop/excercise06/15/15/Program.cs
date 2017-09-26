6393e8b3f210102da702c729a3c1995f1ccdfbad
bpo106 <bpo106bkv@gmail.com>
2017-09-26 15:59:25 +0200

    15 done

--- week04/day01/workshop/excercise06/15/15/Program.cs
+++ week04/day01/workshop/excercise06/15/15/Program.cs
@@ -10,6 +10,13 @@ namespace _15
     {
         static void Main(string[] args)
         {
+            List<Tree> list = new List<Tree>();
+
+            list.Add(new Tree("pine", "green", 20, "male"));
+            list.Add(new Tree("yew", "green", 2, "female"));
+            list.Add(new Tree("apple", "green", 4, "monoecious"));
+            list.Add(new Tree("pear", "green", 15, "monoecious"));
+            list.Add(new Tree("plum", "green", 7, "monoecious"));
         }
     }
 }
