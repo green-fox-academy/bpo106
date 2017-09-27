85b4049b9c87b5b2e914ff9f7aee41e721ee036c
bpo106 <bpo106bkv@gmail.com>
2017-09-27 14:19:49 +0200

    03 done

---
+++ week04/day03/workshop/excercise01/Anagram/Anagram/Anagrams.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anagram
+{
+    public class Anagrams
+    {
+        public bool wordCompare (string s1, string s2)
+        {
+            char[] schar1;
+            char[] schar2;
+            schar1 = s1.ToCharArray();
+            Array.Sort(schar1);
+            schar2 = s2.ToCharArray();
+            Array.Sort(schar2);
+
+            if (schar1.Length == schar2.Length)
+            {
+                for (int i = 0; i < schar1.Length; i++)
+                {
+                    if (schar1[i] != schar2[i])
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ week04/day03/workshop/excercise01/Anagram/AnagramTest.cs/AnagramTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Anagram;
+
+namespace AnagramTest
+{
+    [TestFixture]
+    public class AnagramTest
+    {
+        [Test]
+        public void AnagramEqualityTest()
+        {
+            Anagrams compare = new Anagrams();
+            string word1 = "one plus twelve";
+            string word2 = "two plus eleven";
+
+            Assert.AreEqual(true, compare.wordCompare(word1, word2));
+        }
+
+        [Test]
+        public void AnagramEqualityTestDiffChars()
+        {
+            Anagrams compare = new Anagrams();
+            string word1 = "axaxaxaxaxa";
+            string word2 = "axayayaxaxa";
+
+            Assert.AreEqual(true, compare.wordCompare(word1, word2));
+        }
+
+        [Test]
+        public void AnagramEqualityTestDiffLength()
+        {
+            Anagrams compare = new Anagrams();
+            string word1 = "igazság";
+            string word2 = "gazság";
+
+            Assert.AreEqual(true, compare.wordCompare(word1, word2));
+        }
+    }
+}
