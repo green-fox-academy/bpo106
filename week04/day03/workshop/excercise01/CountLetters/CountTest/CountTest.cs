0b9f85759aa8a995ba488448111c9ef07d577bf5
bpo106 <bpo106bkv@gmail.com>
2017-09-27 15:14:31 +0200

    04 done

---
+++ week04/day03/workshop/excercise01/CountLetters/CountTest/CountTest.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using CountLetters;
+
+namespace CountTest
+{
+    [TestFixture]
+    public class CountTest
+    {
+        [Test]
+        public void dicTest ()
+        {
+            Dictionary<char, int> dic = new Dictionary<char, int>();
+            LetterCounter letcount = new LetterCounter();
+            string test = "Negyvenkettő";
+            dic.Add('N', 1);
+            dic.Add('e', 3);
+            dic.Add('g', 1);
+            dic.Add('y', 1);
+            dic.Add('v', 1);
+            dic.Add('n', 1);
+            dic.Add('k', 1);
+            dic.Add('t', 2);
+            dic.Add('ő', 1);
+
+            Assert.AreEqual(dic, letcount.charLister(test));
+        }
+
+        [Test]
+        public void dicTestDiffNum()
+        {
+            Dictionary<char, int> dic = new Dictionary<char, int>();
+            LetterCounter letcount = new LetterCounter();
+            string test = "Negyvenkettő";
+            dic.Add('N', 1);
+            dic.Add('e', 3);
+            dic.Add('g', 1);
+            dic.Add('y', 1);
+            dic.Add('v', 1);
+            dic.Add('n', 42);
+            dic.Add('k', 1);
+            dic.Add('t', 2);
+            dic.Add('ő', 1);
+
+            Assert.AreEqual(dic, letcount.charLister(test));
+        }
+
+        [Test]
+        public void dicTestDiffChars()
+        {
+            Dictionary<char, int> dic = new Dictionary<char, int>();
+            LetterCounter letcount = new LetterCounter();
+            string test = "Negyvenkettő";
+            dic.Add('N', 1);
+            dic.Add('e', 3);
+            dic.Add('g', 1);
+            dic.Add('y', 1);
+            dic.Add('v', 1);
+            dic.Add('k', 1);
+            dic.Add('t', 2);
+            dic.Add('ő', 1);
+
+            Assert.AreEqual(dic, letcount.charLister(test));
+        }
+    }
+}
---
+++ week04/day03/workshop/excercise01/CountLetters/CountTest/LetterCounter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountLetters
+{
+    public class LetterCounter
+    {
+        public Dictionary<char, int> charLister (string s)
+        {
+            Dictionary<char, int> dictionary = new Dictionary<char, int>();
+            char[] word = s.ToCharArray();
+
+            for (int i = 65; i < 182; i++)
+            {
+                dictionary.Add((char)i, 0);
+            }
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                int count;
+                dictionary.TryGetValue(word[i], out count);
+                dictionary[word[i]] = count + 1;
+            }
+
+            for (int i = 65; i < 182; i++)
+            {
+                int count;
+                dictionary.TryGetValue((char)i, out count);
+                if (count == 0)
+                {
+                    dictionary.Remove((char)i);
+                }
+            }
+
+            return dictionary;
+        }
+    }
+}
