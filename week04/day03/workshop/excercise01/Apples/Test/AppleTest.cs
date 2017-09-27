62ae4be950793c06956ffc9e8ab312bb69b6658d
bpo106 <bpo106bkv@gmail.com>
2017-09-27 13:02:05 +0200

    01 done

---
+++ week04/day03/workshop/excercise01/Apples/Test/AppleTest.cs
@@ -0,0 +1,24 @@
+﻿using Apples;
+using NUnit.Framework;
+
+namespace AppleTest
+{
+    [TestFixture]
+    public class AppleTest
+    {
+        [Test]
+        public void TestAppleCheck()
+        {
+            var apple = new Apple();
+            Assert.AreEqual("apple", apple.GetApple());
+        }
+
+        [Test]
+        public void TestPearCheck()
+        {
+            var apple = new Apple();
+            Assert.AreEqual("pear", apple.GetApple());
+        }
+    }
+
+}
