cf44c69076a0d4a0e59378d959932a84ce7b0396
bpo106 <bpo106bkv@gmail.com>
2017-09-27 17:28:06 +0200

    08 done

---
+++ week04/day03/workshop/excercise01/Animals/Animals/Animals.cs
@@ -0,0 +1,32 @@
+﻿namespace Animals
+{
+    public class Animal
+    {
+        public int Hunger;
+        public int Thirst;
+
+        public Animal()
+        {
+            this.Hunger = 50;
+            this.Thirst = 50;
+        }
+
+        public void eat()
+        {
+            this.Hunger--;
+        }
+
+        public void drink()
+        {
+            this.Thirst--;
+        }
+
+        public void play()
+        {
+            this.Hunger++;
+            this.Thirst++;
+        }
+
+    }
+
+}
---
+++ week04/day03/workshop/excercise01/Animals/TestAnimals/TestAnimals.cs
@@ -0,0 +1,38 @@
+﻿using Animals;
+using NUnit.Framework;
+
+namespace TestAnimals
+{
+    [TestFixture]
+    public class TestAnimals
+    {
+        [Test]
+        public void TestAnimalStatusEat ()
+        {
+            Animal animal = new Animal();
+            int test = animal.Hunger;
+            animal.eat();
+            Assert.AreEqual(test - 1, animal.Hunger);
+        }
+
+        [Test]
+        public void TestAnimalStatusDrink()
+        {
+            Animal animal = new Animal();
+            int test = animal.Thirst;
+            animal.drink();
+            Assert.AreEqual(test - 1, animal.Thirst);
+        }
+
+        [Test]
+        public void TestAnimalStatusPlay()
+        {
+            Animal animal = new Animal();
+            int test1 = animal.Thirst;
+            int test2 = animal.Hunger;
+            animal.play();
+            Assert.AreEqual(test1 + 1, animal.Thirst);
+            Assert.AreEqual(test2 + 1, animal.Hunger);
+        }
+    }
+}
