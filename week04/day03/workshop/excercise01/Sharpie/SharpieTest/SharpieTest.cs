2292b86e6896d25c3adbe7cae78e84f249c19e6c
bpo106 <bpo106bkv@gmail.com>
2017-09-27 16:45:46 +0200

    07 done

---
+++ week04/day03/workshop/excercise01/Sharpie/SharpieTest/SharpieTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sharpies;
+using NUnit.Framework;
+
+namespace SharpieTest
+{
+    [TestFixture]
+    public class SharpieTest
+    {
+        [Test]
+        public void TestInkAmountPositive()
+        {
+            Sharpie sharpie = new Sharpie();
+
+            for (int i = 0; i < 42; i++)
+            {
+                sharpie.Use();
+            }
+
+            Assert.AreEqual(true, sharpie.IsPositive());
+        }
+
+        [Test]
+        public void TestInkAmountNegative()
+        {
+            Sharpie sharpie = new Sharpie();
+
+            for (int i = 0; i < 100; i++)
+            {
+                sharpie.Use();
+            }
+
+            Assert.AreEqual(false, sharpie.IsPositive());
+        }
+    }
+}
