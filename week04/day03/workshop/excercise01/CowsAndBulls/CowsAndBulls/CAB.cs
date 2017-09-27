52e8604cdfcfb2c5e3a13c8c45985e91159d6bc5
bpo106 <bpo106bkv@gmail.com>
2017-09-28 01:44:42 +0200

    09 done (with duplicated digits, they're at the right and the wrong place at the same time)

---
+++ week04/day03/workshop/excercise01/CowsAndBulls/CowsAndBulls/CAB.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CowsAndBulls
+{
+    public class CAB
+    {
+        static Random random = new Random();
+        string state = "playing";
+        int guesses = 0;
+        int randomNum = random.Next(1000, 10000);
+
+        public List<int> BaseNum(int randomNum)
+        {
+            List<int> guess = new List<int>();
+
+            guess.Add((randomNum) / 1000);
+            guess.Add((randomNum % 1000) / 100);
+            guess.Add(((randomNum % 1000) % 100) / 10);
+            guess.Add(((randomNum % 1000) % 100) % 10);
+
+            return guess;
+        }
+
+        public List<int> TempIn(string tempstr)
+        {
+            List<int> temp = new List<int>();
+
+            temp.Add(Int32.Parse(tempstr) / 1000);
+            temp.Add((Int32.Parse(tempstr) % 1000) / 100);
+            temp.Add(((Int32.Parse(tempstr) % 1000) % 100) / 10);
+            temp.Add(((Int32.Parse(tempstr) % 1000) % 100) % 10);
+
+            guesses++;
+
+            return temp;
+        }
+
+        public int Guessing(List<int> guess, List<int> temp)
+        {
+            int cow = 0;
+            int bull = 0;
+
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    if (guess[i] == temp[j]) 
+                    {
+                        if (i == j)
+                        {
+                            cow++;
+                        }
+                        else
+                        {
+                            bull++;
+                        }
+                    }
+                }
+            }
+            for (int i = 0; i < cow; i++)
+            {
+                Console.WriteLine("Cow");
+            }
+
+            for (int i = 0; i < bull; i++)
+            {
+                Console.WriteLine("Bull");
+            }
+
+            if (cow == 4 && bull == 0)
+            {
+                state = "finished";
+                Console.WriteLine("You won! (Guesses: {0})", guesses);
+            }
+            return 10 * cow + bull;
+        }
+    }
+}
---
+++ week04/day03/workshop/excercise01/CowsAndBulls/TestCAB/TestCAB.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CowsAndBulls;
+using NUnit.Framework;
+
+namespace TestCAB
+{
+    [TestFixture]
+    public class TestCAB
+    {
+        [Test]
+        public void TestBaseNum()
+        {
+            CAB cab = new CAB();
+            List<int> list = new List<int> { 6, 7, 7, 4 };
+            Assert.AreEqual(list, cab.BaseNum(6774));
+        }
+
+        [Test]
+        public void TestTempIn ()
+        {
+            CAB cab = new CAB();
+            List<int> list = new List<int>{ 5, 4, 4, 2 };
+            Assert.AreEqual(list, cab.TempIn("5442"));
+        }
+
+        [Test]
+        public void TestNoBullNoCoW ()
+        {
+            CAB cab = new CAB();
+            Assert.AreEqual(0, cab.Guessing(new List<int> { 1, 2, 3, 4 }, new List<int> { 5, 6, 7, 8 }));
+
+        }
+
+        [Test]
+        public void TestBullNoCoW()
+        {
+            CAB cab = new CAB();
+            Assert.AreEqual(4, cab.Guessing(new List<int> { 2, 2, 3, 4 }, new List<int> { 5, 5, 2, 2 }));
+
+        }
+
+        [Test]
+        public void TestCowNoBull()
+        {
+            CAB cab = new CAB();
+            Assert.AreEqual(10, cab.Guessing(new List<int> { 1, 0, 0, 0 }, new List<int> { 1, 2, 3, 4 }));
+
+        }
+
+        [Test]
+        public void TestBullCoW()
+        {
+            CAB cab = new CAB();
+            Assert.AreEqual(22, cab.Guessing(new List<int> { 1, 2, 3, 4 }, new List<int> { 1, 2, 4, 3 }));
+
+        }
+
+        [Test]
+        public void TestWin()
+        {
+            CAB cab = new CAB();
+            Assert.AreEqual(40, cab.Guessing(new List<int> { 1, 2, 3, 4 }, new List<int> { 1, 2, 3, 4 }));
+        }
+    }
+}
