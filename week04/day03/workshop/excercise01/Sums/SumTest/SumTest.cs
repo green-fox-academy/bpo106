afecd3e95b5d6c783df6a043358efedc488393a0
bpo106 <bpo106bkv@gmail.com>
2017-09-27 13:46:01 +0200

    02 done

---
+++ week04/day03/workshop/excercise01/Sums/SumTest/SumTest.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Sums;
+
+namespace SumTest
+{
+    [TestFixture]
+    public class SumTest
+    {
+        [Test]
+        public void TestSumEqualMultiple()
+        {
+            var list = new List<int>();
+
+            list.Add(6);
+            list.Add(9);
+            list.Add(42);
+            list.Add(54);
+
+            var sum = new Sum();
+
+            Assert.AreEqual(111,sum.sumElements(list));
+        }
+
+        [Test]
+        public void TestSumEqualUnique()
+        {
+            var list = new List<int>();
+
+            list.Add(42);
+
+            var sum = new Sum();
+
+            Assert.AreEqual(42, sum.sumElements(list));
+        }
+
+        [Test]
+        public void TestSumEqualEmpty()
+        {
+            var list = new List<int>();
+
+            var sum = new Sum();
+
+            Assert.AreEqual(0, sum.sumElements(list));
+        }
+
+        [Test]
+        public void TestSumEqualNull()
+        {
+            var list = new List<int>();
+
+            list.Add(Int32.Parse(null));
+
+            var sum = new Sum();
+
+            Assert.AreEqual(null, sum.sumElements(list));
+        }
+    }
+}
---
+++ week04/day03/workshop/excercise01/Sums/Sums/Sum.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sums
+{
+    public class Sum
+    {
+        public int sumElements (List<int> list)
+        {
+            for (int i = 1; i < list.Count; i++)
+            {
+                list[0] += list[i];
+            }
+            return list[0];
+        }
+    }
+}
