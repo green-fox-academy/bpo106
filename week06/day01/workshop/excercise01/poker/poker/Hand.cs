95e37db7b7713b40e508518573c1dab71fd4e5d8
bpo106 <bpo106bkv@gmail.com>
2017-10-16 16:27:28 +0200

    sorted lists with three different tests

---
+++ week06/day01/workshop/excercise01/poker/poker/Hand.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace poker
+{
+    public class Hand
+    {
+        int[,] handArray1 = new int[3, 5];
+        int[,] handArray2 = new int[3, 5];
+
+        public int Game(List<int[]> handArray1, List<int[]> handArray2)
+        {
+            handArray1.Sort();
+            handArray2.Sort();
+            for (int i = 4; i >= 0; i--)
+            {
+                if (handArray1[i][0] > handArray2[i][0])
+                {
+                    return 1;
+                }
+                else if (handArray1[i][0] < handArray2[i][0])
+                {
+                    return -1;
+                }
+            }
+            return 0;
+        }
+    }
+}
---
+++ week06/day01/workshop/excercise01/poker/pokertest/Test.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using poker;
+
+namespace pokertest
+{
+    [TestFixture]
+    public class Test
+    {
+        // Első nyer: 1, második nyer: -1, döntetlen: 0
+
+        [Test]
+        public void Test01()
+        {
+            Hand hand = new Hand();
+            List<int[]> hand1 = new List<int[]>() { new int[] { 2, 3, 0 }, new int[] { 3, 2, 0 }, new int[] { 5, 4, 0 }, new int[] { 9, 1, 0 }, new int[] { 12, 2, 0 } };
+            List<int[]> hand2 = new List<int[]>() { new int[] { 2, 1, 0 }, new int[] { 3, 3, 0 }, new int[] { 4, 4, 0 }, new int[] { 8, 1, 0 }, new int[] { 13, 3, 0 } };
+
+            Assert.AreEqual(-1, hand.Game(hand1, hand2));
+        }
+
+        [Test]
+        public void Test02()
+        {
+            Hand hand = new Hand();
+            List<int[]> hand1 = new List<int[]>() { new int[] { 2, 3, 0 }, new int[] { 3, 2, 0 }, new int[] { 5, 4, 0 }, new int[] { 9, 1, 0 }, new int[] { 13, 2, 0 } };
+            List<int[]> hand2 = new List<int[]>() { new int[] { 2, 1, 0 }, new int[] { 3, 3, 0 }, new int[] { 4, 4, 0 }, new int[] { 8, 1, 0 }, new int[] { 13, 3, 0 } };
+
+            Assert.AreEqual(1, hand.Game(hand1, hand2));
+        }
+
+        [Test]
+        public void Test03()
+        {
+            Hand hand = new Hand();
+            List<int[]> hand1 = new List<int[]>() { new int[] { 2, 3, 0 }, new int[] { 3, 2, 0 }, new int[] { 9, 1, 0 }, new int[] { 13, 2, 0 }, new int[] { 5, 4, 0 } };
+            List<int[]> hand2 = new List<int[]>() { new int[] { 2, 1, 0 }, new int[] { 3, 3, 0 }, new int[] { 4, 4, 0 }, new int[] { 8, 1, 0 }, new int[] { 13, 3, 0 } };
+
+            Assert.AreEqual(1, hand.Game(hand1, hand2));
+        }
+    }
+}
