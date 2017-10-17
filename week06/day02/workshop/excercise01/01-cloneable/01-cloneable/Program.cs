fa6dfd845080936d9b54c8e3a0eb699417955fb8
bpo106 <bpo106bkv@gmail.com>
2017-10-17 13:37:41 +0200

    Task 01 done.

---
+++ week06/day02/workshop/excercise01/01-cloneable/01-cloneable/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_cloneable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student john = new Student("John Doe", 20, "male", "BME");
+            Student johnTheClone = john.Clone();
+        }
+    }
+}
---
+++ week06/day02/workshop/excercise01/01-cloneable/01-cloneable/Student.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_cloneable
+{
+    public class Student : ICloneable
+    {
+        private string name;
+        private int age;
+        private string gender;
+        private string previousOrganization;
+        private int skippedDays;
+
+        public Student(string name, int age, string gender, string previousOrganization)
+        {
+            this.name = name;
+            this.age = age;
+            this.gender = gender;
+            this.previousOrganization = previousOrganization;
+            skippedDays = 0;
+        }
+
+        public void GetGoal()
+        {
+            Console.WriteLine("Be a junior software developer.");
+        }
+
+        public void Introduce()
+        {
+            Console.WriteLine("Hi, I'm {0}, a {1} year old {2} from {3} who skipped {4} days from the course already.", name, age, gender, previousOrganization, skippedDays);
+        }
+
+        public void SkipDays(int numberOfDays)
+        {
+            skippedDays += numberOfDays;
+        }
+
+        public Student Clone()
+        {
+            Student cloned = new Student(name, age, gender, previousOrganization);
+            return cloned;
+        }
+    }
+}
