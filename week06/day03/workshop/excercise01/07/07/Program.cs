aca8a66d0316df46040cebfd9f444ed32f7980f5
bpo106 <bpo106bkv@gmail.com>
2017-10-18 14:31:31 +0200

    Task 07 done.

---
+++ week06/day03/workshop/excercise01/07/07/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace _07
+{
+    class Program
+    {
+        static void aiLambda(string[] s)
+        {
+            var ai = s.Where(x => x[x.Length - 1] == 'I' && x[0] == 'A').Select(x => x);
+
+            foreach (var element in ai)
+            {
+                Console.Write(element + " ");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            string[] cities = { "ROME", "LONDON", "NAIROBI", "CALIFORNIA", "ZURICH", "NEW DELHI", "AMSTERDAM", "ABU DHABI", "PARIS" };
+
+            foreach (string city in cities) {
+                Console.Write(city + " ");
+            }
+            Console.WriteLine();
+            aiLambda(cities);
+
+            Console.ReadLine();
+        }
+    }
+}
