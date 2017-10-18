e88239f16272bb6394808ec2b68dbe0af14b355e
bpo106 <bpo106bkv@gmail.com>
2017-10-18 11:21:13 +0200

    Task 01 done both with lambda and query.

---
+++ week06/day03/workshop/excercise01/01/01/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01
+{
+    class Program
+    {
+        static void evenSql (int[] n)
+        {
+            var even = from element in n
+                       where element % 2 == 0
+                       select element;
+
+            foreach (var element in even)
+            {
+                Console.Write(element + " ");
+            }
+        }
+
+        static void evenLambda(int[] n)
+        {
+            IEnumerable<int> even = n.Where(x => x % 2 == 0);
+
+            foreach (var element in even)
+            {
+                Console.Write(element + " ");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int[] n = { 1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14 };
+
+            evenSql(n);
+            Console.WriteLine();
+            evenLambda(n);
+
+            Console.ReadLine();
+        }
+    }
+}
