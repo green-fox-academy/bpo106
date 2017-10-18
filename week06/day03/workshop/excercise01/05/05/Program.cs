f0f9ecd45e596440114701ef3fbe3ea23898b0e9
bpo106 <bpo106bkv@gmail.com>
2017-10-18 13:52:40 +0200

    Task 05 done.

---
+++ week06/day03/workshop/excercise01/05/05/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05
+{
+    class Program
+    {
+        static void frequencyLambda(int[] n)
+        {
+            var frequency = n.GroupBy(x => x).Select(x => new { Number = x.Key, Count = x.Count() });
+
+            foreach (var element in frequency)
+            {
+                Console.WriteLine(element.Number + ": " + element.Count);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int[] n = new int[] { 5, 9, 1, 2, 3, 7, 5, 6, 7, 3, 7, 6, 8, 5, 4, 9, 6, 2 };
+
+            frequencyLambda(n);
+
+            Console.ReadLine();
+        }
+    }
+}
