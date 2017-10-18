8de2f2a62d55166574a58dba32f253aa2fa2461b
bpo106 <bpo106bkv@gmail.com>
2017-10-18 14:17:31 +0200

    Task 06 done.

---
+++ week06/day03/workshop/excercise01/06/06/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06
+{
+    class Program
+    {
+        static void frequencyLambda(string s)
+        {
+            s = s.ToLower();
+            var frequency = s.Where(x => x != '.' && x != ',' && x != ' ' && x != '-').GroupBy(x => x).Select(x => new { Character = x.Key, Count = x.Count() });
+        
+            foreach (var element in frequency)
+            {
+                Console.WriteLine(element.Character + ": " + element.Count);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            string s = "Etelka, a tapír elmegy anyagmennyiség-becslőnek.";
+
+            Console.WriteLine(s);
+            frequencyLambda(s);
+
+            Console.ReadLine();
+        }
+    }
+}
