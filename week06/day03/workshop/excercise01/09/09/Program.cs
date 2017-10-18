21ead3d5df8fdf1eea01498dcceeba530055039e
bpo106 <bpo106bkv@gmail.com>
2017-10-18 15:45:42 +0200

    Task 09 done.

--- week06/day03/workshop/excercise01/09/09/Program.cs
+++ week06/day03/workshop/excercise01/09/09/Program.cs
@@ -9,22 +9,26 @@ namespace _09
         static void toStringQuery(char[] s)
         {
             var stringElements = from element in s
-                                select element.ToString();
+                                 select element.ToString();
 
-            Console.WriteLine(String.Join("", stringElements.Select(x => x.ToString().ToArray())));
+            string str = "";
+            foreach (var element in stringElements)
+            {
+                str += element;
+            }
+            Console.WriteLine(str);
         }
 
         static void toStringLambda(char[] s)
         {
-            Console.WriteLine(String.Join("", s.Select(x => x.ToString().ToArray())));
+            Console.WriteLine(String.Join("", s.Select(x => x.ToString()).ToArray()));
         }
 
         static void Main(string[] args)
         {
-            char[] s = { 'B', 'a', 's', 'z', 'ó', 'd', 'j', 'á', 'l', ' ', 'm', 'e', 'g', ',', ' ', 'K', 'D', 'N', 'P', '!' };
+            char[] s = { 'á', 'r', 'v', 'í', 'z', 't', 'ű', 'r', 'ő', ' ', 't', 'ü', 'k', 'ö', 'r', 'f', 'ú', 'r', 'ó', 'g', 'é', 'p' };
 
             toStringQuery(s);
-            Console.WriteLine();
             toStringLambda(s);
 
             Console.ReadLine();
