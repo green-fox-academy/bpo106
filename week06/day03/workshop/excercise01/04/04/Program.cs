b1bdb94ed682f75362490811d75e98ff1fb1a42c
bpo106 <bpo106bkv@gmail.com>
2017-10-18 13:07:47 +0200

    Task 04 done.

---
+++ week06/day03/workshop/excercise01/04/04/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04
+{
+    class Program
+    {
+        static void squareQuery(int[] n)
+        {
+            var square = from element in n
+                         where element * element > 20
+                         select element;
+
+            foreach (var element in square)
+            {
+                Console.Write(element + " ");
+            }
+        }
+
+        static void squareLambda(int[] n)
+        {
+            IEnumerable<int> square = n.Where(x => x * x > 20);
+
+            foreach (var element in square)
+            {
+                Console.Write(element + " ");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int[] n = new[] { 3, 9, 2, 8, 6, 5 };
+
+            squareQuery(n);
+            Console.WriteLine();
+            squareLambda(n);
+
+            Console.ReadLine();
+        }
+    }
+}
