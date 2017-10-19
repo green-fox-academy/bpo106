885d0afd9f958f374d0c0f37497fd187a9b502ca
bpo106 <bpo106bkv@gmail.com>
2017-10-19 11:50:08 +0200

    Task 01 done with refactoring.

--- week06/day04/workshop/excercise01/01-parkinglot/01-parkinglot/Program.cs
+++ week06/day04/workshop/excercise01/01-parkinglot/01-parkinglot/Program.cs
@@ -8,6 +8,22 @@ namespace _01_parkinglot
 {
     class Program
     {
+        static public void CountAndPrint(List<Car> list, int i)
+        {
+            var frequency = list.Select(x => x.nameAndColor[i]).GroupBy(x => x).Select(x => new { Key = x.Key, Count = x.Count() });
+            foreach (var element in frequency)
+            {
+                if (i == 0)
+                {
+                    Console.WriteLine(Enum.GetName(typeof(Types), element.Key) + ": " + element.Count);
+                }
+                else
+                {
+                    Console.WriteLine(Enum.GetName(typeof(Colors), element.Key) + ": " + element.Count);
+                }
+            }
+        }
+
         static void Main(string[] args)
         {
             Random random = new Random();
@@ -19,9 +35,9 @@ namespace _01_parkinglot
             }
 
             Console.WriteLine("Types:");
-            DoSomethingWithThoseCars.CountAndPrint(list, 0);
+            CountAndPrint(list, 0);
             Console.WriteLine("\nColors:");
-            DoSomethingWithThoseCars.CountAndPrint(list, 1);
+            CountAndPrint(list, 1);
             Console.ReadLine();
         }
     }
