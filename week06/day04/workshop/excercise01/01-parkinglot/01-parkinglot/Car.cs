dd25660b07bce4e20a0563df89be77135c86220b
bpo106 <bpo106bkv@gmail.com>
2017-10-19 11:45:45 +0200

    Task 01 done.

---
+++ week06/day04/workshop/excercise01/01-parkinglot/01-parkinglot/Car.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_parkinglot
+{
+    public enum Types
+    {
+        VAZ,
+        UAZ,
+        GAZ,
+        KAMAZ,
+        IZH,
+        GolAZ,
+        BelAZ,
+        MAZ,
+        ZIL
+    }
+
+    public enum Colors
+    {
+        green,
+        red,
+        white,
+        yellow,
+        black,
+        blue,
+        brown,
+        grey,
+        wtf
+    }
+
+    public class Car
+    {
+        public int[] nameAndColor = new int[2];
+
+        public Car(int name, int color)
+        {
+            nameAndColor[0] = name;
+            nameAndColor[1] = color;
+        }
+    }
+}
---
+++ week06/day04/workshop/excercise01/01-parkinglot/01-parkinglot/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_parkinglot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            List<Car> list = new List<Car>();
+
+            for (int i = 0; i < 256; i++)
+            {
+                list.Add(new Car(random.Next(0, 9), random.Next(0, 9)));
+            }
+
+            Console.WriteLine("Types:");
+            DoSomethingWithThoseCars.CountAndPrint(list, 0);
+            Console.WriteLine("\nColors:");
+            DoSomethingWithThoseCars.CountAndPrint(list, 1);
+            Console.ReadLine();
+        }
+    }
+}
