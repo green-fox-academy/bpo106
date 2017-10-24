f2081b837ec3076c027e7e69b3cfc266530751e7
bpo106 <bpo106bkv@gmail.com>
2017-10-24 13:04:36 +0200

    T done.

---
+++ week07/day01/workshop/excercise01/WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApplication1.Controllers
+{
+
+    public class HomeController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            //return View();
+            //return new JsonResult(new { Id = 10, Content = "Hello World!" });
+            return Content("Hello World!");
+        }
+
+        [Route("hello")]
+        public IActionResult Hello(string content)
+        {
+            return Content("Hello!");
+        }
+    }
+}
