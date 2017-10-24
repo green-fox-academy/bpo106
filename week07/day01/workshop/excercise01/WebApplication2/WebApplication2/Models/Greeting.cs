a0fa15c62295cdcd44fded3279adce9e09d5595f
bpo106 <bpo106bkv@gmail.com>
2017-10-24 13:54:19 +0200

    revert

---
+++ week07/day01/workshop/excercise01/WebApplication2/WebApplication2/Models/Greeting.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication2.Models
+{
+    public class Greeting
+    {
+        public long Id;
+        public string Content;
+    }
+}
