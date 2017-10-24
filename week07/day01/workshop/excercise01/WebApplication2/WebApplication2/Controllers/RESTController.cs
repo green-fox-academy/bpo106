8236a96a0927e48faba8a2b9cba5efe51c263f94
bpo106 <bpo106bkv@gmail.com>
2017-10-24 14:32:25 +0200

    REST web app done.

---
+++ week07/day01/workshop/excercise01/WebApplication2/WebApplication2/Controllers/RESTController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApplication2.Controllers
+{
+    [Route("api")]
+    public class RESTController : Controller
+    {
+        // GET: /<controller>/
+        
+        [Route("greeting")]
+        public IActionResult Greeting()
+        {
+            Greeting obj = new Greeting() { Id = 1, Content = "Hello World!" };
+            return new JsonResult(obj);
+        }
+    }
+}
