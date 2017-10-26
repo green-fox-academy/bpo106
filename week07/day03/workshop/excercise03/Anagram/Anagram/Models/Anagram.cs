126c368598b0f2942b38088b389d208f67b3848e
bpo106 <bpo106bkv@gmail.com>
2017-10-26 17:17:44 +0200

    Base of anagram task built (files, connections between files).

---
+++ week07/day03/workshop/excercise03/Anagram/Anagram/Models/Anagram.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Anagram.Models
+{
+    public class Anagram
+    {
+        public string word1;
+        public string word2;
+    }
+}
