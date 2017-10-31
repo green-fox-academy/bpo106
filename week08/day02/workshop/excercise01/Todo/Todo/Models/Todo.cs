1c31a7d9445b7c0f067b19eda4482aa6f5a3f7f2
bpo106 <bpo106bkv@gmail.com>
2017-10-31 13:44:11 +0100

    First steps of the Todo app. Enjoy.

---
+++ week08/day02/workshop/excercise01/Todo/Todo/Models/Todo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Todo.Models
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool IsUrgent { get; set; }
+        public bool IsDone { get; set; }
+
+        public Todo()
+        {
+            IsUrgent = false;
+            IsDone = false;
+        }
+    }
+}
