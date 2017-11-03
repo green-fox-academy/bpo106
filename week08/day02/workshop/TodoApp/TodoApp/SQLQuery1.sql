5d93aef83421fd0ef3e3aa7d76d7d6b70a1dc734
bpo106 <bpo106bkv@gmail.com>
2017-11-03 14:20:53 +0100

    I love committing.

---
+++ week08/day02/workshop/TodoApp/TodoApp/SQLQuery1.sql
@@ -0,0 +1,6 @@
+﻿SET IDENTITY_INSERT Todos ON;
+INSERT INTO Todos (Id, IsDone, IsUrgent, Title) VALUES (1, 1, 0, 'First Task');
+INSERT INTO Todos (Id, IsDone, IsUrgent, Title) VALUES (2, 0, 1, 'Second Task');
+INSERT INTO Todos (Id, IsDone, IsUrgent, Title) VALUES (3, 0, 0, 'Third Task');
+INSERT INTO Todos (Id, IsDone, IsUrgent, Title) VALUES (4, 0, 0, 'Fourth Task');
+SET IDENTITY_INSERT Todos OFF;
