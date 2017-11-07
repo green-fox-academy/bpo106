4a184ded0b0b97245daa1348a5a9910faffa242f
bpo106 <bpo106bkv@gmail.com>
2017-11-07 14:15:06 +0100

    Random commit!

--- week09/day02/workshop/excercise01/Frontend/Frontend/Controllers/HomeController.cs
+++ week09/day02/workshop/excercise01/Frontend/Frontend/Controllers/HomeController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -14,6 +10,35 @@ namespace Frontend.Controllers
         public IActionResult Index()
         {
             return File("index.html", "text/html");
+        }
+
+        [HttpGet]
+        [Route("/doubling")]
+        public IActionResult Doubling(int? input)
+        {
+            if (input != null)
+            {
+                return Json(new { received = input, result = input * 2 });
+            }
+            return Json(new { error = "Please provide an input!" });
+        }
+
+        [HttpGet]
+        [Route("/greeter")]
+        public IActionResult Greeter(string name, string title)
+        {
+            if (name == null || title == null)
+            {
+                return Json(new { error = "Please provide a name and a title!" });
+            }
+            return Json(new { welcome_message = "Oh, hi there " + name +", my dear " + title + "!" });
+        }
+
+        [HttpGet]
+        [Route("/appenda/{appendable}")]
+        public IActionResult Appenda(string appendable)
+        {
+            return Json(new { appended = appendable + "a" });
         }
     }
 }
