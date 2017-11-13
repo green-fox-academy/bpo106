a43fd6c70a100aa2fc8694947f652d00e56182ac
bpo106 <bpo106bkv@gmail.com>
2017-11-13 11:50:50 +0100

    First steps of Bus List Manager.

---
+++ week10/day01-day04/workshop/excercise01/BusListManager/BusListManager/Models/Bus.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BusListManager.Models
+{
+    public class Bus
+    {
+        public int Id { get; set; }
+        public int Number { get; set; }
+        public int FormerNumber { get; set; }
+        public string LicensePlate { get; set; }
+        public string FormerLicensePlate { get; set; }
+        public string Type { get; set; }
+        public string Depot { get; set; }
+        public DateTime DateOfGet { get; set; }
+        public DateTime DateOfWithdrawal { get; set; }
+        public int Kilometer { get; set; }
+        public bool IsInOperation { get; set; }
+        public string Comments { get; set; }
+    }
+}
