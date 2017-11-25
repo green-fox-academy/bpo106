996413121b3a4d0dc7dc07124f0045598378e9aa
bpo106 <bpo106bkv@gmail.com>
2017-11-25 10:53:30 +0100

    Base of Reddit

---
+++ week08/day05/workshop/excercise01/Reddit/Reddit/Controllers/PostController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Reddit.Controllers
+{
+    [Route("")]
+    public class PostController : Controller
+    {
+        [Route("")]
+        [Route("/posts")]
+        [HttpGet]
+        public IActionResult Posts()
+        {
+            return View();
+        }
+
+        [Route("/posts/add")]
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+    }
+}
