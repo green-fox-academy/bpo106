af26f7848a52eefdf715b65b31cadbe0f3bbcf2d
bpo106 <bpo106bkv@gmail.com>
2018-01-15 09:29:22 +0100

    Task.

---
+++ exam/task/Task/Task/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Task
+{
+    class Program
+    {
+        //Task 2: Create a function that reverses a string
+
+        //With a simple for loop
+        static string Reverse (string s)
+        {
+            string reversed = "";
+            for (int i = s.Length - 1; i >= 0; i--)
+            {
+                reversed += s[i];
+            }
+            return reversed;
+        }
+
+        //With Array.Reverse()
+        static string Reverse2 (string s)
+        {
+            char[] array = s.ToCharArray();
+            Array.Reverse(array);
+            return new string(array);
+        }
+
+        //In a bit wtf way
+        static string Reverse3 (string s)
+        {
+            for (int i = s.Length - 1; i >= 0; i--)
+            {
+                s += s[i];
+            }
+            return s.Substring(s.Length / 2, s.Length / 2);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Reverse("asdasdasd"));
+            Console.WriteLine(Reverse2("asdasdasd"));
+            Console.WriteLine(Reverse3("asdasdasd"));
+            Console.ReadLine();
+        }
+    }
+}
